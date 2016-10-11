6a7e148cb20078e410b9070e1e632b82f6ec9ab9
chris-anderson67 <chrisja67@gmail.com>
2016-10-11 18:54:48 -0400

    added support for mailto links, decreased recursive calls with continue statements in find_emails loop

--- find_email_addresses.py
+++ find_email_addresses.py
@@ -8,12 +8,14 @@
 from bs4 import BeautifulSoup
 import urllib2
 from urlparse import urljoin
+from urlparse import urlparse
 from sets import Set
 import sys
 import re
 
 # Regex from http://scraping.pro/email-validation-regexes/
 CONST_EMAIL_REGEX = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+DEBUG = True
 
 urls = Set() # visited urls
 emails = Set() # collected emails
@@ -33,36 +35,50 @@ def open_url(url):
 
 # url: current page to parse
 # domain: domain to crawl
-# i: recursion depth
-def find_emails(url, domain, i):
-    if (i >= 2):
-        return
-
+# debug: if true will print each page it visists
+def find_emails(url, domain, debug):
     html = open_url(url)
-    if (html == False):
-        return
+
+    #Base cases
+    if (html == False): return
+    if (url in urls): return
+    if (debug): print url
+
+    urls.add(url)
     soup = BeautifulSoup(html, "html.parser")
 
     # print all plaintext emails on page
     for t in soup.find_all(text=re.compile(CONST_EMAIL_REGEX)):
         emails.add(t.string)
 
+    #find all links, follow http, extract mailto
     for a in soup.find_all('a'):
         href = a.get('href')
-        # make sure there is a link
-        if not href:
+        if not href: continue
+        if ("mailto" in href):
+            href = href[7:]
+            emails.add(href)
             continue
-        if (domain in href):
-            new_url = urljoin(url, href)
-            if ("http" in new_url):
-                find_emails(new_url, domain, i + 1)
-                # print "JOINED: " + new_url
-        # print a.string, href 
+
+        # Find links that dont leave domain, aren't visited
+        if (domain not in href): continue
+        new_url = urljoin(url, href)
+        if (domain not in urlparse(new_url).netloc): continue
+        if ("http" not in new_url): continue
+        if (new_url in urls): continue
+
+        find_emails(new_url, domain, debug)
 
 def main():
-    domain = str(sys.argv[1]); # Add checks for no cla
-    url = "https://www." + domain # Assumes domain given
-    find_emails(url, domain, 0);
+    # Not propper Python c.l.a. in python
+    # For production code use get-opt
+    if (len(sys.argv) < 2): 
+        print "provide a domain"
+        sys.exit(1)
+
+    domain = str(sys.argv[1]);
+    url = "https://www." + domain # Assumes domain given (web.com)
+    find_emails(url, domain, DEBUG);
     for e in emails:
         print e
 
